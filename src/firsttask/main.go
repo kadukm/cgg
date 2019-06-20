31d9f9a4a00543713820f3efd2b760c3813828c8
Mikhail <kaduk.mikhail@gmail.com>
2019-06-20 18:27:51 +0500

    rewrite solustion to using gg

--- src/firsttask/main.go
+++ src/firsttask/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"image"
 	"image/color"
-	"math"
 
+	"github.com/fogleman/gg"
 	"github.com/kadukm/cgg/src/utility"
-	"github.com/llgcode/draw2d/draw2dimg"
 )
 
 const (
 	width  = 1000
 	height = 600
 
-	a float64 = -100
-	b float64 = 100
+	a float64 = -10
+	b float64 = 15
+
+	minXXAxeStepLength = 32
+	minYYAxeStepLength = 32
 
 	fileName = "firsttask.png"
 )
@@ -24,22 +26,21 @@ var (
 )
 
 func f(x float64) float64 {
-	return math.Sin(x) * x
+	return x * x * x
 }
 
 func main() {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	utility.Fill(img, color.White)
 
-	gc := draw2dimg.NewGraphicContext(img)
-	gc.SetStrokeColor(color.Black)
+	gc := gg.NewContextForImage(img)
 	gc.SetLineWidth(1)
-	gc.BeginPath()
 
 	findMinMax()
+	drawAxes(gc)
 	drawF(gc)
 
-	draw2dimg.SaveToPngFile(fileName, img)
+	gc.SavePNG(fileName)
 }
 
 func findMinMax() {
@@ -58,7 +59,9 @@ func findMinMax() {
 	}
 }
 
-func drawF(gc *draw2dimg.GraphicContext) {
+func drawF(gc *gg.Context) {
+	gc.SetColor(color.RGBA{153, 12, 12, 255})
+
 	yy := cartesianYToScreen(f(a))
 	gc.MoveTo(0, float64(yy))
 
@@ -71,6 +74,31 @@ func drawF(gc *draw2dimg.GraphicContext) {
 
 	gc.Stroke()
 }
-func drawAxes(gc *draw2dimg.GraphicContext) {
+
+func drawAxes(gc *gg.Context) {
+	gc.SetColor(color.Black)
+
+	xx0 := cartesianXToScreen(0)
+	yy0 := cartesianYToScreen(0)
+
+	gc.MoveTo(float64(xx0), 0)
+	gc.LineTo(float64(xx0), height)
+
+	gc.MoveTo(0, float64(yy0))
+	gc.LineTo(width, float64(yy0))
+
+	// minXAxeStepLength := screenXXToCartesian(minXXAxeStepLength) - screenXXToCartesian(0)
+	// xAxeStepLength := math.Max(1, minXAxeStepLength)
+
+	// for x := float64(0); x < math.Max(math.Abs(a), math.Abs(b)); x += xAxeStepLength {
+	// 	xx_right := cartesianXToScreen(x)
+	// 	xx_left := cartesianXToScreen(-x)
+	// 	qp.drawLine(xx_right, yy0 - 2, xx_right, yy0 + 2)
+	// 	qp.drawLine(xx_left, yy0 - 2, xx_left, yy0 + 2)
+	// 	qp.drawText(QRect(xx_right - 10, yy0 + 10, 20, 20), Qt.AlignCenter, str(x))
+	// 	qp.drawText(QRect(xx_left - 10, yy0 + 10, 20, 20), Qt.AlignCenter, str(-x))
+	// }
+
+	gc.Stroke()
 
 }
